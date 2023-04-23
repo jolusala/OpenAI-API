b094446241217570a6d562e4de98ef3ede73a0a3
Jose Luis Salazar <jose.luissalacast@gmail.com>
2023-04-22 21:49:56 -0500

    Text Completions

---
+++ app.js
@@ -0,0 +1,27 @@
+const API_KEY = "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"
+
+async function fetchData() {
+  const response = await fetch('https://api.openai.com/v1/completions',{
+    method: 'POST',
+    headers : {
+        Authorization : `Bearer ${API_KEY}`,
+        'Content-Type': 'application/json'
+    },
+
+    body: JSON.stringify({
+        model: "text-davinci-003",
+        prompt: "Hello, how are you?",
+        max_tokens: 7
+
+    })
+
+  })
+
+  const data = await response.json()
+  console.log(data)
+  console.log("fetching data")
+}
+
+fetchData()
+
+
